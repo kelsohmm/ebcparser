8d1defa59211712468348d49992572497b38b376
Bogusz <stefanczykb@gmail.com>
2018-04-17 23:42:01 +0200

    added CurrencyExchange model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class CurrencyPair(models.Model):
+    base_currency = models.CharField(max_length=3, blank=False)
+    target_currency = models.CharField(max_length=3, blank=False)
+    date = models.CharField(max_length=10, blank=False) # dates as chars
+    exchange_rate = models.FloatField(blank=False)

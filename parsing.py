bb09dcba819f268ccb8215e7baf77b5a9813e164
Bogusz <stefanczykb@gmail.com>
2018-04-17 23:03:23 +0200

    added support for parsing feeds with multiple entries

--- parsing.py
+++ parsing.py
@@ -3,10 +3,14 @@ import feedparser
 
 def parse_currency_feed(rss_source):
     feed = feedparser.parse(rss_source)
-    date = feed.entries[0].date.split('T')[0]
+    return [
+        parse_feed_entry(entry)
+        for entry in feed.entries
+    ]
 
-    entry = feed.entries[0]
+
+def parse_feed_entry(entry):
+    date = entry.date.split('T')[0]
     target_currency = entry['cb_targetcurrency']
     [exchange_rate, base_currency] = [word.strip() for word in entry['cb_exchangerate'].split('\n')]
-
     return (base_currency, target_currency, exchange_rate, date)
--- test/test_parsing.py
+++ test/test_parsing.py
@@ -1,8 +1,14 @@
 from unittest import TestCase
 from parsing import parse_currency_feed
-from test.currency_feed_strings import raw_one_exchange_feed
+from test.currency_feed_strings import *
 
 
 class TestParsing(TestCase):
-    def test_should_parse_currency_pair_for_string_with_one_pair(self):
-        assert parse_currency_feed(raw_one_exchange_feed) == ('EUR', 'CZK', '25.254', '2018-04-17')
+    def test_should_parse_currency_feed_for_string_with_one_entry(self):
+        assert parse_currency_feed(raw_one_entry_feed)[0] == ('EUR', 'CZK', '25.254', '2018-04-17')
+
+    def test_should_parse_currency_feed_for_string_with_multiple_entries(self):
+        result = parse_currency_feed(raw_three_entries_feed)
+        assert result[0] == ('EUR', 'CZK', '25.254', '2018-04-17')
+        assert result[1] == ('EUR', 'CZK', '25.265', '2018-04-16')
+        assert result[2] == ('EUR', 'CZK', '25.307', '2018-04-13')

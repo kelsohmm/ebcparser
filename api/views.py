bca80f8799f7af8ed7d26b1c378681690cb12d71
Bogusz <stefanczykb@gmail.com>
2018-04-18 01:21:15 +0200

    Revert "sync commit of unfinished work"
    This reverts commit a076dd7

--- api/views.py
+++ api/views.py
@@ -1,8 +1,3 @@
-from rest_framework import generics
-from api.models import CurrencyPair
-from api.serializers import CurrencyPairSerializer
+from django.shortcuts import render
 
-
-class DetailsView(generics.RetrieveAPIView):
-    queryset = CurrencyPair.objects.all()
-    serializer_class = CurrencyPairSerializer
+# Create your views here.

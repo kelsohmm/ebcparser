ac5eeefac6a286eabf177921a720fe6bf0b9a0f3
Bogusz <stefanczykb@gmail.com>
2018-04-17 23:06:49 +0200

    added parsing test for full feed of sgd currency

--- test/test_parsing.py
+++ test/test_parsing.py
@@ -12,3 +12,12 @@ class TestParsing(TestCase):
         assert result[0] == ('EUR', 'CZK', '25.254', '2018-04-17')
         assert result[1] == ('EUR', 'CZK', '25.265', '2018-04-16')
         assert result[2] == ('EUR', 'CZK', '25.307', '2018-04-13')
+
+    def test_should_parse_full_feed_with_other_currency(self):
+        result = parse_currency_feed(raw_full_feed_currency_sgd)
+        assert result[0] == ('EUR', 'SGD', '1.6192', '2018-04-17')
+        assert result[1] == ('EUR', 'SGD', '1.6221', '2018-04-16')
+        assert result[2] == ('EUR', 'SGD', '1.6158', '2018-04-13')
+        assert result[3] == ('EUR', 'SGD', '1.6155', '2018-04-12')
+        assert result[4] == ('EUR', 'SGD', '1.6204', '2018-04-11')
+

5a6aa99bbd455431ba94583d3adb11f8ec37a30f
Bogusz <stefanczykb@gmail.com>
2018-04-18 22:20:36 +0200

    refactored test_view_currency_exchange.py for per-test data initialization

--- test/test_view_currency_exchange.py
+++ test/test_view_currency_exchange.py
@@ -14,12 +14,15 @@ class ViewTestCase(TestCase):
         'date': '2018-04-17',
         'exchange_rate': 1.6192,
     }
+    currency_pair_strings = [
+        ('EUR', 'SGD', '1.6192', '2018-04-17')
+    ]
 
     def setUp(self):
         self.client = APIClient()
-        CurrencyPair.from_strings_tuple(('EUR', 'SGD', '1.6192', '2018-04-17')).save()
 
     def test_api_can_get_a_single_currency_pair(self):
+        self.insert_currency_pairs(self.currency_pair_strings[0:1])
         router_args = {'base_currency': 'EUR', 'target_currency': 'SGD'}
         response = self._get_response(router_args)
 
@@ -27,11 +30,16 @@ class ViewTestCase(TestCase):
         self.assertEqual(dict(response.data[0]), self.rest_data_single)
 
     def test_api_can_get_a_single_currency_pair_when_currencies_lower_case(self):
+        self.insert_currency_pairs(self.currency_pair_strings[0:1])
         router_args = {'base_currency': 'eur', 'target_currency': 'sgd'}
         response = self._get_response(router_args)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(dict(response.data[0]), self.rest_data_single)
+
+    def insert_currency_pairs(self, string_tuples):
+        for string_tuple in string_tuples:
+            CurrencyPair.from_strings_tuple(string_tuple).save()
 
     def _get_response(self, router_args):
         return self.client.get(

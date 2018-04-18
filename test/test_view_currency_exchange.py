3436dbacfdf6d0962dda268a05aab59a1eb8977c
Bogusz <stefanczykb@gmail.com>
2018-04-18 22:43:10 +0200

    extracted ViewTestCase superclass

--- test/test_view_currency_exchange.py
+++ test/test_view_currency_exchange.py
@@ -6,7 +6,25 @@ from rest_framework import status
 from api.models import CurrencyPair
 
 
-class ViewTestCase(TestCase):
+class ViewTestCaseCore(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def _assertDataEqualToExpected(self, response_data, expected):
+        for response_ordered_dict, expected_dict in zip(response_data, expected):
+            self.assertDictEqual(dict(response_ordered_dict), expected_dict)
+
+    def _insert_currency_pairs(self, string_tuples):
+        for string_tuple in string_tuples:
+            CurrencyPair.from_strings_tuple(string_tuple).save()
+
+    def _get_response(self, router_args):
+        return self.client.get(
+            reverse('details',
+                    kwargs=router_args), format="json")
+
+
+class SingleCurrencyViewTestCase(ViewTestCaseCore):
     rest_responses = [
         {'id': 1, 'base_currency': 'EUR', 'target_currency': 'SGD', 'exchange_rate': 1.6192, 'date': '2018-04-17'},
         {'id': 2, 'base_currency': 'EUR', 'target_currency': 'SGD', 'exchange_rate': 1.45, 'date': '2018-04-16'},
@@ -18,9 +36,6 @@ class ViewTestCase(TestCase):
         ('EUR', 'SGD', '1.0', '2018-04-15'),
     ]
     router_args_sgd = {'base_currency': 'EUR', 'target_currency': 'SGD'}
-
-    def setUp(self):
-        self.client = APIClient()
 
     def test_api_can_get_a_single_currency_pair(self):
         self._insert_currency_pairs(self.currency_pair_strings[0:1])
@@ -43,16 +58,3 @@ class ViewTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self._assertDataEqualToExpected(response.data, self.rest_responses)
 
-    def _assertDataEqualToExpected(self, response_data, expected):
-        self.assertEqual(len(response_data), len(expected))
-        for response_ordered_dict, expected_dict in zip(response_data, expected):
-            self.assertDictEqual(dict(response_ordered_dict), expected_dict)
-
-    def _insert_currency_pairs(self, string_tuples):
-        for string_tuple in string_tuples:
-            CurrencyPair.from_strings_tuple(string_tuple).save()
-
-    def _get_response(self, router_args):
-        return self.client.get(
-            reverse('details',
-                    kwargs=router_args), format="json")
